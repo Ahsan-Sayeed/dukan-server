3f2f81d1d481bd78306105ecf6c81d658fd080cf
Ahsan-Sayeed <ahsansayeedsaad@gmail.com>
2023-06-28 15:00:04 +0600

    env deleted

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // const oldDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.48fmuml.mongodb.net/?retryWrites=true&w=majority`;
-const newDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect(process.env.NEW_DB)
+// const newDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect("mongodb+srv://dukan:iLU9aUBG07M51s2F@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log('database connected successfully');
 })
