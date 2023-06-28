cc7135105c37ff727d3feb1d97062a5ffcdd8a57
Ahsan-Sayeed <ahsansayeedsaad@gmail.com>
2023-06-28 15:13:42 +0600

    sdf

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-// const oldDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.48fmuml.mongodb.net/?retryWrites=true&w=majority`;
-// const newDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect("mongodb+srv://dukan:iLU9aUBG07M51s2F@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority")
+
+mongoose.connect("mongodb+srv://dukan:2LJzIKg8XFisnKf5@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log('database connected successfully');
 })
