1e28c342feb6a8e57fc18ec4213aa651c5d2085b
Ahsan-Sayeed <ahsansayeedsaad@gmail.com>
2023-06-28 14:37:53 +0600

    env variable

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // const oldDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.48fmuml.mongodb.net/?retryWrites=true&w=majority`;
 const newDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect(newDb)
+mongoose.connect(process.env.NEW_DB)
 .then(()=>{
     console.log('database connected successfully');
 })
