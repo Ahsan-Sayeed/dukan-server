c28b225f289bf99638d7ea8c8fc5a3164d02bd6f
Ahsan-Sayeed <ahsansayeedsaad@gmail.com>
2023-06-28 14:30:35 +0600

    new database connected

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.48fmuml.mongodb.net/?retryWrites=true&w=majority`)
+// const oldDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.48fmuml.mongodb.net/?retryWrites=true&w=majority`;
+const newDb = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.plkdc5k.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(newDb)
 .then(()=>{
     console.log('database connected successfully');
 })
